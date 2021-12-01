a8c765f312595d0722e80010d4ea679a52f81db0
Ayana Monroe <ayanam@live.unc.edu>
2021-11-30 20:10:24 -0500

    I think this is what that means

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -93,7 +93,14 @@ export const tenTimesFifty = () => {
  *    everyEven([1, 1, 0, 1, 1], x => x === 1)  <--  returns false
  */
 export const everyEven = (arr, test) => {
-
+    for (let i = 0; i < arr.length; i++){
+        if (i%2 == 0){
+            if (test(arr[i])){
+                return true
+            }
+        }
+    }
+    return false
 };
 
 
